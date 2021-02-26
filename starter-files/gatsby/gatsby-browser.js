4e442303aa59f995341f409fec720cbec0a8575e
PatrickWitmer <patrick@witmerdesigns.com>
2021-02-26 08:39:50 -0700

    This Friday is on!

--- starter-files/gatsby/gatsby-browser.js
+++ starter-files/gatsby/gatsby-browser.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Layout from './src/components/Layout';
 
+/* eslint-disable react/jsx-props-no-spreading */
+
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
 }
