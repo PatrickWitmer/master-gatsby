f2c45eb1d998b817983a6876a9dbe8998e5843ce
PatrickWitmer <patrick@witmerdesigns.com>
2021-03-01 15:17:41 -0700

    Sanity lost, but Sanity.io is started.

--- starter-files/sanity/schemas/pizza.js
+++ starter-files/sanity/schemas/pizza.js
@@ -1,4 +1,5 @@
 import { MdLocalPizza as icon } from 'react-icons/md';
+import PriceInput from '../components/PriceInput';
 
 export default {
   // Computer Name
@@ -34,9 +35,35 @@ export default {
       name: 'price',
       title: 'Price',
       type: 'number',
-      description: 'Price of the pizza in cents',
       validation: (Rule) => Rule.min(1000),
-      // TODO: Add custom input component
+      description: 'Price of the pizza in cents',
+      inputComponent: PriceInput,
+    },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
     },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // 1. Filter undefined toppings out.
+      const tops = Object.values(toppings).filter(Boolean);
+      // 2. Return the preview object for the pizza.
+      return {
+        title,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
 };
