d9cd36e548c127eb22d4fb0f572256c4c83ed424
PatrickWitmer <patrick@witmerdesigns.com>
2021-03-24 09:47:15 -0600

    hooked up sourcing data from sanity.io

--- starter-files/gatsby/gatsby-config.js
+++ starter-files/gatsby/gatsby-config.js
@@ -1,7 +1,24 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
 module.exports = {
   siteMetadata: {
     title: 'Slick’s Slices',
     siteUrl: 'https://gatsby.pizza',
-    description: 'The best pizza place in  Mabe Village.',
+    description: 'The best pizza place in Mabe Village.',
   },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // This is the name of the plugin you are adding
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: '4jhcjptw',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
